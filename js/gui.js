3ab5c7f9ac83a5801947450c5063f5be38e453cd
Adriex <adrielchagay@gmail.com>
2018-02-21 15:30:07 -0300

    Solucion de errores y Añadido el logo

--- js/gui.js
+++ js/gui.js
@@ -1,16 +1,22 @@
-var inputHorario = '<div>'+
-'<div class="four columns"><input class="day u-full-width" type="text" placeholder="Dia"/></div>'+
-'<div class="four columns"><input class="StartHour u-full-width" type="text" placeholder="Hora de Inicio"/></div>'+
-'<div class="four columns"><input class="FinishHour u-full-width" type="text" placeholder="Hora de Finalizacion"/></div></div>';
+var selectDia = '<div class="four columns">'+'<select class="day u-full-width">'+
+'<option value="Default">Dia</option><option value="Lunes">Lunes</option><option value="Martes">Martes</option>'+
+'<option value="Miercoles">Miercoles</option><option value="Jueves">Jueves</option><option value="Viernes">Viernes</option>'+
+'<option value="Sabado">Sabado</option></select></div>';
 
-var inputComision = '<div class="row comissions"><div class="three columns">'+
+
+var inputHorario = '<div class="row schedule">'+selectDia+
+'<div class="four columns"><input class="startHour u-full-width" type="time" placeholder="Hora de Inicio"/></div>'+
+'<div class="four columns"><input class="finishHour u-full-width" type="time" placeholder="Hora de Finalizacion"/></div></div>';
+
+
+var inputComision = '<div class="row comissions"><div class="nameComision three columns">'+
 '<input class="comission" type="text" placeholder="Comision"/></div>'+
-'<div class="nine columns hour"><div class="row">'+inputHorario+
+'<div class="nine columns hour horarios"><div class="row schedules">'+inputHorario+
 '</div><button class="u-full-width addHour boton">Añadir Horario</button></div></div>';
 
-var inputMateria = '<div class="inputs"><div class="row">'+
-'<input id="nameSubject" class="u-full-width" type="text" placeholder="Materia"/></div>'+
-'<div>'+inputComision+'</div><button class="u-full-width addCommission boton">Añadir Comisión</button></div>';
+var inputMateria = '<div class="inputs"><div class="row nameMateria">'+
+'<input id="nameSubject" class="nameSubject u-full-width" type="text" placeholder="Materia"/>'+
+'</div><div class="row comisiones">'+inputComision+'</div><button class="u-full-width addCommission boton">Añadir Comisión</button></div>';
 
 $(document).on('click',".addHour", function(){
   $(this).prev().append(inputHorario);
